396ade5c013ceca4073fa3830972c22d0684e0c1
Adam Mertz <adammertz@gmail.com>
2018-02-06 20:42:41 -0600

    Removed unnecessary methods

--- Lab01/Node/Node.go
+++ Lab01/Node/Node.go
@@ -8,11 +8,3 @@ type Node struct {
 func NewNode(value int) *Node {
 	return &Node{value, nil}
 }
-
-func (n *Node) GetValue() int {
-	return n.Value
-}
-
-func (n *Node) setValue(value int) {
-	n.Value = value
-}
